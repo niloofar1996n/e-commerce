d8c4b7a756d929403554256ce8bd98df52c72606
niloofar1996n <niloofar1996n@gmail.com>
2021-08-22 16:29:06 +0430

    adding products to homepage

---
+++ src/component/pages/homepage/homePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './homePage.scss'
+import Directory from "../../dictory/directory";
+
+
+const HomePage = () => {
+    return(
+        <div className='homepage'>
+            <Directory  />
+        </div>
+    )
+}
+
+export default HomePage;
